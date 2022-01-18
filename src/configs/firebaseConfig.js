5a39f2eebd4c0e6ed5f64340066778c782bd48ca
sandeepssingh <sandeep.singh2632@gmail.com>
2022-01-18 14:15:02 +0530

    projectID changes

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -1,8 +1,8 @@
 var firebaseConfig = {
-  projectId: "coviddashboard-3c9a6",
   apiKey: "AIzaSyB-HlY-oZ2XBQZJ-nUWVC4xNhAxlzV9qpo",
   authDomain: "coviddashboard-3c9a6.firebaseapp.com",
-  databaseURL: "https://coviddashboard-3c9a6-default-rtdb.asia-southeast1.firebasedatabase.app",  
+  databaseURL: "https://coviddashboard-3c9a6-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "coviddashboard-3c9a6",
   storageBucket: "coviddashboard-3c9a6.appspot.com",
   messagingSenderId: "434505147901",
   appId: "1:434505147901:web:bd0f342e0e6cb597c42594",
